ba73db811dd04d515ed51fa4f2e0d69b53f2fa2e
XGNoir95 <shuhridabrar2016@gmail.com>
2025-09-01 21:30:08 +0600

    Initializing Tailwind

---
+++ MetroFlow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MetroFlow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MetroFlow/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./Views/**/*.cshtml", "./Pages/**/*.cshtml", "./wwwroot/**/*.js"],
+    theme: {
+        container: { center: true, padding: "1rem" },
+        extend: {},
+    },
+    plugins: [],
+};
