88178aa49919a53b6f8c4b60ff1ecc619d153f50
XGNoir95 <shuhridabrar2016@gmail.com>
2025-09-06 17:05:58 +0600

    Fixing exception issue and integrating a mock login page

--- MetroFlow/Program.cs
+++ MetroFlow/Program.cs
@@ -1,23 +1,26 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using MetroFlow.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Enable Razor Runtime Compilation in Development
-#if DEBUG
-builder.Services
-    .AddControllersWithViews()
-    .AddRazorRuntimeCompilation();
-#else
+// Add services to the container.
 builder.Services.AddControllersWithViews();
-#endif
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddSingleton<MetroFlow.Services.MailService>();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
