bf5bdf74dbd95eea3a26119844c9206acae7404f
Zubaer Mahmud Shihab <zubaer.mahmud31@gmail.com>
2025-09-06 08:35:06 +0600

    Added few models

---
+++ MetroFlow/Models/Station.cs
@@ -0,0 +1,11 @@
+﻿namespace MetroFlow.Models
+{
+    public class Station
+    {
+        public string StationName { get; set; } = "";
+        public int StationId { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+
+    }
+}
---
+++ MetroFlow/Models/Transaction.cs
@@ -0,0 +1,12 @@
+﻿namespace MetroFlow.Models
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public int CardId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        public string Description { get; set; } = "";
+
+    }
+}
