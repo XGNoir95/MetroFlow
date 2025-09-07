cadf22293081a1d0f10333a03db24c77312675c6
mahadir <mahadirmrh04@gmail.com>
2025-09-08 04:18:41 +0600

    Adding Schedule and Chatbot

---
+++ MetroFlow/Controllers/MetroController.cs
@@ -0,0 +1,29 @@
+﻿using MetroFlow.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MetroFlow.Controllers
+{
+    public class MetroController : Controller
+    {
+        public IActionResult Schedule()
+        {
+            // Example schedules - replace with real DB data
+            var weekdaySchedules = new List<ScheduleModel>
+            {
+                new ScheduleModel { Direction = "Northbound", StartTime = "7:30 AM", EndTime = "9:40 PM" },
+                new ScheduleModel { Direction = "Southbound", StartTime = "7:45 AM", EndTime = "9:55 PM" }
+            };
+
+            var fridaySchedules = new List<ScheduleModel>
+            {
+                new ScheduleModel { Direction = "Northbound", StartTime = "3:00 PM", EndTime = "10:20 PM", Frequency = "Every 20 mins" },
+                new ScheduleModel { Direction = "Southbound", StartTime = "3:10 PM", EndTime = "10:30 PM", Frequency = "Every 25 mins" }
+            };
+
+            ViewBag.WeekdaySchedules = weekdaySchedules;
+            ViewBag.FridaySchedules = fridaySchedules;
+
+            return View();
+        }
+    }
+}
---
+++ MetroFlow/Models/ScheduleModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MetroFlow.Models
+{
+    public class ScheduleModel
+    {
+        public string Direction { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string Frequency { get; set; } // optional for weekdays
+    }
+}
