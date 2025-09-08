7af6f5d728842b363a1812b20ed643e1ae7f495b
XGNoir95 <shuhridabrar2016@gmail.com>
2025-09-08 22:33:19 +0600

    Making schedule backend oriented

---
+++ MetroFlow/Models/Schedule.cs
@@ -0,0 +1,13 @@
+namespace MetroFlow.Models
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+        public string DayType { get; set; } // "Weekday", "Friday"
+        public string Direction { get; set; } // "UttaraToMotijheel", "MotijheelToUttara"
+        public TimeSpan FirstTrain { get; set; }
+        public TimeSpan LastTrain { get; set; }
+        public string PeakFrequency { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
